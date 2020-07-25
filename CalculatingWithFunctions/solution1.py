79caef2149fddbf3a12dc97018fd57562a5e0e6a
RyanSamman <RyanSamman@outlook.com>
2020-07-25 08:14:13 +0300

    Calculating With Functions - Unoptimal solution

---
+++ CalculatingWithFunctions/solution1.py
@@ -0,0 +1,74 @@
+# Unoptimal Solution
+# https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/python
+
+def zero(funct=None): #your code here
+    val = 0
+    if funct is None:
+        return val
+    return funct(val)
+
+def one(funct=None): #your code here
+    val = 1
+    if funct is None:
+        return val
+    return funct(val)
+
+def two(funct=None): #your code here
+    val = 2
+    if funct is None:
+        return val
+    return funct(val)
+
+def three(funct=None): #your code here
+    val = 3
+    if funct is None:
+        return val
+    return funct(val)
+
+def four(funct=None): #your code here
+    val = 4
+    if funct is None:
+        return val
+    return funct(val)
+
+def five(funct=None): #your code here
+    val = 5
+    if funct is None:
+        return val
+    return funct(val)
+
+def six(funct=None): #your code here
+    val = 6
+    if funct is None:
+        return val
+    return funct(val)
+
+def seven(funct=None): #your code here
+    val = 7
+    if funct is None:
+        return val
+    return funct(val)
+
+def eight(funct=None): #your code here
+    val = 8
+    if funct is None:
+        return val
+    return funct(val)
+
+def nine(funct=None): #your code here
+    val = 9
+    if funct is None:
+        return val
+    return funct(val)
+
+def plus(i2): #your code here
+    return lambda i1: i1 + i2
+
+def minus(i2): #your code here
+    return lambda i1: i1 - i2
+
+def times(i2): #your code here
+    return lambda i1: i1 * i2
+
+def divided_by(i2): #your code here
+    return lambda i1: i1 // i2
